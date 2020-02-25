a76afd211f2d1019a164d081f0ed4cb0990d9f85
Hussamuddin Nasir <nasir@netlab.uky.edu>
2020-02-25 10:06:56 -0500

    Added new sniffer program

---
+++ sniffer/sniff.c
@@ -0,0 +1,504 @@
+
+#include<stdio.h>	//For standard things
+#include<stdlib.h>	//malloc
+#include<string.h>	//memset
+#include <errno.h>	//error
+#include<netinet/ip_icmp.h>	//Provides declarations for icmp header
+#include<netinet/udp.h>	//Provides declarations for udp header
+#include<netinet/tcp.h>	//Provides declarations for tcp header
+#include<netinet/ip.h>	//Provides declarations for ip header
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+
+#include <asm/types.h>
+#include <linux/net_tstamp.h>
+#include <linux/errqueue.h>
+#include <linux/sockios.h>
+
+
+#ifndef SO_TIMESTAMPING
+# define SO_TIMESTAMPING         37
+# define SCM_TIMESTAMPING        SO_TIMESTAMPING
+#endif
+
+#ifndef SO_TIMESTAMPNS
+# define SO_TIMESTAMPNS 35
+#endif
+
+
+
+void ProcessPacket(unsigned char* , int);
+void print_ip_header(unsigned char* , int);
+void print_tcp_packet(unsigned char* , int);
+void print_udp_packet(unsigned char * , int);
+void print_icmp_packet(unsigned char* , int);
+void PrintData (unsigned char* , int);
+
+int sock_raw;
+FILE *logfile;
+int tcp=0,udp=0,icmp=0,others=0,igmp=0,total=0,i,j;
+struct sockaddr_in source,dest;
+
+static void bail(const char *error)
+{
+	printf("%s: %s\n", error, strerror(errno));
+	exit(1);
+}
+
+
+int main()
+{
+	int saddr_size , data_size;
+	struct sockaddr saddr;
+	struct in_addr in;
+	int val;
+	socklen_t len;
+	int so_timestamping_flags = 0;
+	int so_timestamp = 0;
+	int so_timestampns = 0;
+	int siocgstamp = 0;
+	int siocgstampns = 0;
+	struct ifreq device;
+	struct ifreq hwtstamp;
+	struct hwtstamp_config hwconfig, hwconfig_requested;
+	struct sockaddr_in addr;
+	struct ip_mreq imr;
+	struct in_addr iaddr;
+	struct timeval next;
+	char *interface;
+
+
+	interface = "enp0s31f6";
+
+	so_timestamping_flags |= SOF_TIMESTAMPING_TX_HARDWARE;
+	//so_timestamping_flags |= SOF_TIMESTAMPING_TX_SOFTWARE;
+	so_timestamping_flags |= SOF_TIMESTAMPING_RX_HARDWARE;
+	//so_timestamping_flags |= SOF_TIMESTAMPING_RX_SOFTWARE;
+	//so_timestamping_flags |= SOF_TIMESTAMPING_SOFTWARE;
+	so_timestamping_flags |= SOF_TIMESTAMPING_RAW_HARDWARE;
+
+
+	
+	unsigned char *buffer = (unsigned char *)malloc(65536); //Its Big!
+	
+	logfile=fopen("/tmp/log.txt","w");
+	if(logfile==NULL) printf("Unable to create file.");
+	printf("Starting...\n");
+	//Create a raw socket that shall sniff
+	sock_raw = socket(AF_INET , SOCK_RAW , IPPROTO_TCP);
+	if(sock_raw < 0)
+	{
+		printf("Socket Error\n");
+		return 1;
+	}
+
+
+	memset(&device, 0, sizeof(device));
+	strncpy(device.ifr_name, interface, sizeof(device.ifr_name));
+	if (ioctl(sock_raw, SIOCGIFADDR, &device) < 0)
+		bail("getting interface IP address");
+
+	memset(&hwtstamp, 0, sizeof(hwtstamp));
+	strncpy(hwtstamp.ifr_name, interface, sizeof(hwtstamp.ifr_name));
+	hwtstamp.ifr_data = (void *)&hwconfig;
+	memset(&hwconfig, 0, sizeof(hwconfig));
+	hwconfig.tx_type =
+		(so_timestamping_flags & SOF_TIMESTAMPING_TX_HARDWARE) ?
+		HWTSTAMP_TX_ON : HWTSTAMP_TX_OFF;
+	hwconfig.rx_filter =
+		(so_timestamping_flags & SOF_TIMESTAMPING_RX_HARDWARE) ?
+		HWTSTAMP_FILTER_PTP_V1_L4_SYNC : HWTSTAMP_FILTER_NONE;
+	hwconfig_requested = hwconfig;
+	if (ioctl(sock_raw, SIOCSHWTSTAMP, &hwtstamp) < 0) {
+		if ((errno == EINVAL || errno == ENOTSUP) &&
+		    hwconfig_requested.tx_type == HWTSTAMP_TX_OFF &&
+		    hwconfig_requested.rx_filter == HWTSTAMP_FILTER_NONE)
+			printf("SIOCSHWTSTAMP: disabling hardware time stamping not possible\n");
+		else
+			bail("SIOCSHWTSTAMP");
+	}
+	printf("SIOCSHWTSTAMP: tx_type %d requested, got %d; rx_filter %d requested, got %d\n",
+	       hwconfig_requested.tx_type, hwconfig.tx_type,
+	       hwconfig_requested.rx_filter, hwconfig.rx_filter);
+
+
+	if (so_timestamping_flags &&
+	setsockopt(sock_raw, SOL_SOCKET, SO_TIMESTAMPING,
+		   &so_timestamping_flags,
+		   sizeof(so_timestamping_flags)) < 0)
+	bail("setsockopt SO_TIMESTAMPING");
+	
+	/* verify socket options */
+	len = sizeof(val);
+	if (getsockopt(sock_raw, SOL_SOCKET, SO_TIMESTAMPING, &val, &len) < 0) {
+		printf("%s: %s\n", "getsockopt SO_TIMESTAMPING",
+		       strerror(errno));
+	} else {
+		printf("SO_TIMESTAMPING %d\n", val);
+		if (val != so_timestamping_flags)
+			printf("   not the expected value %d\n",
+			       so_timestamping_flags);
+	}
+	while(1)
+	{
+		saddr_size = sizeof saddr;
+		//Receive a packet
+		data_size = recvfrom(sock_raw , buffer , 65536 , 0 , &saddr , &saddr_size);
+		if(data_size <0 )
+		{
+			printf("Recvfrom error , failed to get packets\n");
+			return 1;
+		}
+		//Now process the packet
+		ProcessPacket(buffer , data_size);
+	}
+	close(sock_raw);
+	printf("Finished");
+	return 0;
+}
+
+void ProcessPacket(unsigned char* buffer, int size)
+{
+	//Get the IP Header part of this packet
+	struct iphdr *iph = (struct iphdr*)buffer;
+	++total;
+	switch (iph->protocol) //Check the Protocol and do accordingly...
+	{
+		case 1:  //ICMP Protocol
+			++icmp;
+			//PrintIcmpPacket(Buffer,Size);
+			break;
+		
+		case 2:  //IGMP Protocol
+			++igmp;
+			break;
+		
+		case 6:  //TCP Protocol
+			++tcp;
+			print_tcp_packet(buffer , size);
+			break;
+		
+		case 17: //UDP Protocol
+			++udp;
+			print_udp_packet(buffer , size);
+			break;
+		
+		default: //Some Other Protocol like ARP etc.
+			++others;
+			break;
+	}
+	printf("TCP : %d   UDP : %d   ICMP : %d   IGMP : %d   Others : %d   Total : %d\r",tcp,udp,icmp,igmp,others,total);
+}
+
+void print_ip_header(unsigned char* Buffer, int Size)
+{
+	unsigned short iphdrlen;
+		
+	struct iphdr *iph = (struct iphdr *)Buffer;
+	iphdrlen =iph->ihl*4;
+	
+	memset(&source, 0, sizeof(source));
+	source.sin_addr.s_addr = iph->saddr;
+	
+	memset(&dest, 0, sizeof(dest));
+	dest.sin_addr.s_addr = iph->daddr;
+	
+	fprintf(logfile,"\n");
+	fprintf(logfile,"IP Header\n");
+	fprintf(logfile,"   |-IP Version        : %d\n",(unsigned int)iph->version);
+	fprintf(logfile,"   |-IP Header Length  : %d DWORDS or %d Bytes\n",(unsigned int)iph->ihl,((unsigned int)(iph->ihl))*4);
+	fprintf(logfile,"   |-Type Of Service   : %d\n",(unsigned int)iph->tos);
+	fprintf(logfile,"   |-IP Total Length   : %d  Bytes(Size of Packet)\n",ntohs(iph->tot_len));
+	fprintf(logfile,"   |-Identification    : %d\n",ntohs(iph->id));
+	//fprintf(logfile,"   |-Reserved ZERO Field   : %d\n",(unsigned int)iphdr->ip_reserved_zero);
+	//fprintf(logfile,"   |-Dont Fragment Field   : %d\n",(unsigned int)iphdr->ip_dont_fragment);
+	//fprintf(logfile,"   |-More Fragment Field   : %d\n",(unsigned int)iphdr->ip_more_fragment);
+	fprintf(logfile,"   |-TTL      : %d\n",(unsigned int)iph->ttl);
+	fprintf(logfile,"   |-Protocol : %d\n",(unsigned int)iph->protocol);
+	fprintf(logfile,"   |-Checksum : %d\n",ntohs(iph->check));
+	fprintf(logfile,"   |-Source IP        : %s\n",inet_ntoa(source.sin_addr));
+	fprintf(logfile,"   |-Destination IP   : %s\n",inet_ntoa(dest.sin_addr));
+}
+
+void print_tcp_packet(unsigned char* Buffer, int Size)
+{
+	unsigned short iphdrlen;
+	
+	struct iphdr *iph = (struct iphdr *)Buffer;
+	iphdrlen = iph->ihl*4;
+	
+	struct tcphdr *tcph=(struct tcphdr*)(Buffer + iphdrlen);
+			
+	fprintf(logfile,"\n\n***********************TCP Packet*************************\n");	
+		
+	print_ip_header(Buffer,Size);
+		
+	fprintf(logfile,"\n");
+	fprintf(logfile,"TCP Header\n");
+	fprintf(logfile,"   |-Source Port      : %u\n",ntohs(tcph->source));
+	fprintf(logfile,"   |-Destination Port : %u\n",ntohs(tcph->dest));
+	fprintf(logfile,"   |-Sequence Number    : %u\n",ntohl(tcph->seq));
+	fprintf(logfile,"   |-Acknowledge Number : %u\n",ntohl(tcph->ack_seq));
+	fprintf(logfile,"   |-Header Length      : %d DWORDS or %d BYTES\n" ,(unsigned int)tcph->doff,(unsigned int)tcph->doff*4);
+	//fprintf(logfile,"   |-CWR Flag : %d\n",(unsigned int)tcph->cwr);
+	//fprintf(logfile,"   |-ECN Flag : %d\n",(unsigned int)tcph->ece);
+	fprintf(logfile,"   |-Urgent Flag          : %d\n",(unsigned int)tcph->urg);
+	fprintf(logfile,"   |-Acknowledgement Flag : %d\n",(unsigned int)tcph->ack);
+	fprintf(logfile,"   |-Push Flag            : %d\n",(unsigned int)tcph->psh);
+	fprintf(logfile,"   |-Reset Flag           : %d\n",(unsigned int)tcph->rst);
+	fprintf(logfile,"   |-Synchronise Flag     : %d\n",(unsigned int)tcph->syn);
+	fprintf(logfile,"   |-Finish Flag          : %d\n",(unsigned int)tcph->fin);
+	fprintf(logfile,"   |-Window         : %d\n",ntohs(tcph->window));
+	fprintf(logfile,"   |-Checksum       : %d\n",ntohs(tcph->check));
+	fprintf(logfile,"   |-Urgent Pointer : %d\n",tcph->urg_ptr);
+	fprintf(logfile,"\n");
+	// fprintf(logfile,"                        DATA Dump                         ");
+	// fprintf(logfile,"\n");
+		
+	// fprintf(logfile,"IP Header\n");
+	// PrintData(Buffer,iphdrlen);
+		
+	// fprintf(logfile,"TCP Header\n");
+	// PrintData(Buffer+iphdrlen,tcph->doff*4);
+		
+	// fprintf(logfile,"Data Payload\n");	
+	// PrintData(Buffer + iphdrlen + tcph->doff*4 , (Size - tcph->doff*4-iph->ihl*4) );
+						
+	fprintf(logfile,"\n###########################################################");
+}
+
+void print_udp_packet(unsigned char *Buffer , int Size)
+{
+	
+	unsigned short iphdrlen;
+	
+	struct iphdr *iph = (struct iphdr *)Buffer;
+	iphdrlen = iph->ihl*4;
+	
+	struct udphdr *udph = (struct udphdr*)(Buffer + iphdrlen);
+	
+	fprintf(logfile,"\n\n***********************UDP Packet*************************\n");
+	
+	print_ip_header(Buffer,Size);			
+	
+	fprintf(logfile,"\nUDP Header\n");
+	fprintf(logfile,"   |-Source Port      : %d\n" , ntohs(udph->source));
+	fprintf(logfile,"   |-Destination Port : %d\n" , ntohs(udph->dest));
+	fprintf(logfile,"   |-UDP Length       : %d\n" , ntohs(udph->len));
+	fprintf(logfile,"   |-UDP Checksum     : %d\n" , ntohs(udph->check));
+	
+	// fprintf(logfile,"\n");
+	// fprintf(logfile,"IP Header\n");
+	// PrintData(Buffer , iphdrlen);
+		
+	// fprintf(logfile,"UDP Header\n");
+	// PrintData(Buffer+iphdrlen , sizeof udph);
+		
+	// fprintf(logfile,"Data Payload\n");	
+	// PrintData(Buffer + iphdrlen + sizeof udph ,( Size - sizeof udph - iph->ihl * 4 ));
+	
+	fprintf(logfile,"\n###########################################################");
+}
+
+void print_icmp_packet(unsigned char* Buffer , int Size)
+{
+	unsigned short iphdrlen;
+	
+	struct iphdr *iph = (struct iphdr *)Buffer;
+	iphdrlen = iph->ihl*4;
+	
+	struct icmphdr *icmph = (struct icmphdr *)(Buffer + iphdrlen);
+			
+	fprintf(logfile,"\n\n***********************ICMP Packet*************************\n");	
+	
+	print_ip_header(Buffer , Size);
+			
+	fprintf(logfile,"\n");
+		
+	fprintf(logfile,"ICMP Header\n");
+	fprintf(logfile,"   |-Type : %d",(unsigned int)(icmph->type));
+			
+	if((unsigned int)(icmph->type) == 11) 
+		fprintf(logfile,"  (TTL Expired)\n");
+	else if((unsigned int)(icmph->type) == ICMP_ECHOREPLY) 
+		fprintf(logfile,"  (ICMP Echo Reply)\n");
+	fprintf(logfile,"   |-Code : %d\n",(unsigned int)(icmph->code));
+	fprintf(logfile,"   |-Checksum : %d\n",ntohs(icmph->checksum));
+	//fprintf(logfile,"   |-ID       : %d\n",ntohs(icmph->id));
+	//fprintf(logfile,"   |-Sequence : %d\n",ntohs(icmph->sequence));
+	fprintf(logfile,"\n");
+
+	// fprintf(logfile,"IP Header\n");
+	// PrintData(Buffer,iphdrlen);
+		
+	// fprintf(logfile,"UDP Header\n");
+	// PrintData(Buffer + iphdrlen , sizeof icmph);
+		
+	// fprintf(logfile,"Data Payload\n");	
+	// PrintData(Buffer + iphdrlen + sizeof icmph , (Size - sizeof icmph - iph->ihl * 4));
+	
+	fprintf(logfile,"\n###########################################################");
+}
+
+void PrintData (unsigned char* data , int Size)
+{
+	
+	for(i=0 ; i < Size ; i++)
+	{
+		if( i!=0 && i%16==0)   //if one line of hex printing is complete...
+		{
+			fprintf(logfile,"         ");
+			for(j=i-16 ; j<i ; j++)
+			{
+				if(data[j]>=32 && data[j]<=128)
+					fprintf(logfile,"%c",(unsigned char)data[j]); //if its a number or alphabet
+				
+				else fprintf(logfile,"."); //otherwise print a dot
+			}
+			fprintf(logfile,"\n");
+		} 
+		
+		if(i%16==0) fprintf(logfile,"   ");
+			fprintf(logfile," %02X",(unsigned int)data[i]);
+				
+		if( i==Size-1)  //print the last spaces
+		{
+			for(j=0;j<15-i%16;j++) fprintf(logfile,"   "); //extra spaces
+			
+			fprintf(logfile,"         ");
+			
+			for(j=i-i%16 ; j<=i ; j++)
+			{
+				if(data[j]>=32 && data[j]<=128) fprintf(logfile,"%c",(unsigned char)data[j]);
+				else fprintf(logfile,".");
+			}
+			fprintf(logfile,"\n");
+		}
+	}
+}
+
+static void print_ts(struct msghdr *msg, int res,
+			char *data,
+			int sock, int recvmsg_flags,
+			int siocgstamp, int siocgstampns)
+{
+	struct sockaddr_in *from_addr = (struct sockaddr_in *)msg->msg_name;
+	struct cmsghdr *cmsg;
+	struct timeval tv;
+	struct timespec ts;
+	struct timeval now;
+
+	gettimeofday(&now, 0);
+
+	printf("%ld.%06ld: received %s data, %d bytes from %s, %zu bytes control messages\n",
+	       (long)now.tv_sec, (long)now.tv_usec,
+	       (recvmsg_flags & MSG_ERRQUEUE) ? "error" : "regular",
+	       res,
+	       inet_ntoa(from_addr->sin_addr),
+	       msg->msg_controllen);
+	for (cmsg = CMSG_FIRSTHDR(msg);
+	     cmsg;
+	     cmsg = CMSG_NXTHDR(msg, cmsg)) {
+		printf("   cmsg len %zu: ", cmsg->cmsg_len);
+		switch (cmsg->cmsg_level) {
+		case SOL_SOCKET:
+			printf("SOL_SOCKET ");
+			switch (cmsg->cmsg_type) {
+			case SO_TIMESTAMP: {
+				struct timeval *stamp =
+					(struct timeval *)CMSG_DATA(cmsg);
+				printf("SO_TIMESTAMP %ld.%06ld",
+				       (long)stamp->tv_sec,
+				       (long)stamp->tv_usec);
+				break;
+			}
+			case SO_TIMESTAMPNS: {
+				struct timespec *stamp =
+					(struct timespec *)CMSG_DATA(cmsg);
+				printf("SO_TIMESTAMPNS %ld.%09ld",
+				       (long)stamp->tv_sec,
+				       (long)stamp->tv_nsec);
+				break;
+			}
+			case SO_TIMESTAMPING: {
+				struct timespec *stamp =
+					(struct timespec *)CMSG_DATA(cmsg);
+				printf("SO_TIMESTAMPING ");
+				printf("SW %ld.%09ld ",
+				       (long)stamp->tv_sec,
+				       (long)stamp->tv_nsec);
+				stamp++;
+				/* skip deprecated HW transformed */
+				stamp++;
+				printf("HW raw %ld.%09ld",
+				       (long)stamp->tv_sec,
+				       (long)stamp->tv_nsec);
+				break;
+			}
+			default:
+				printf("type %d", cmsg->cmsg_type);
+				break;
+			}
+			break;
+		case IPPROTO_IP:
+			printf("IPPROTO_IP ");
+			switch (cmsg->cmsg_type) {
+			case IP_RECVERR: {
+				struct sock_extended_err *err =
+					(struct sock_extended_err *)CMSG_DATA(cmsg);
+				printf("IP_RECVERR ee_errno '%s' ee_origin %d => %s",
+					strerror(err->ee_errno),
+					err->ee_origin,
+#ifdef SO_EE_ORIGIN_TIMESTAMPING
+					err->ee_origin == SO_EE_ORIGIN_TIMESTAMPING ?
+					"bounced packet" : "unexpected origin"
+#else
+					"probably SO_EE_ORIGIN_TIMESTAMPING"
+#endif
+					);
+
+				break;
+			}
+			case IP_PKTINFO: {
+				struct in_pktinfo *pktinfo =
+					(struct in_pktinfo *)CMSG_DATA(cmsg);
+				printf("IP_PKTINFO interface index %u",
+					pktinfo->ipi_ifindex);
+				break;
+			}
+			default:
+				printf("type %d", cmsg->cmsg_type);
+				break;
+			}
+			break;
+		default:
+			printf("level %d type %d",
+				cmsg->cmsg_level,
+				cmsg->cmsg_type);
+			break;
+		}
+		printf("\n");
+	}
+
+	if (siocgstamp) {
+		if (ioctl(sock, SIOCGSTAMP, &tv))
+			printf("   %s: %s\n", "SIOCGSTAMP", strerror(errno));
+		else
+			printf("SIOCGSTAMP %ld.%06ld\n",
+			       (long)tv.tv_sec,
+			       (long)tv.tv_usec);
+	}
+	if (siocgstampns) {
+		if (ioctl(sock, SIOCGSTAMPNS, &ts))
+			printf("   %s: %s\n", "SIOCGSTAMPNS", strerror(errno));
+		else
+			printf("SIOCGSTAMPNS %ld.%09ld\n",
+			       (long)ts.tv_sec,
+			       (long)ts.tv_nsec);
+	}
+}
